1ba3c13e4271894d4fbdbc070594615958b304f1
Sören Meier <soeren@s-me.ch>
2022-04-06 21:17:19 +0200

    first commit 1.6.14

---
+++ README.md
@@ -0,0 +1,9 @@
+[![crates.io](https://img.shields.io/crates/v/mmdbus)](https://crates.io/crates/mmdbus)
+[![docs.rs](https://img.shields.io/docsrs/mmdbus)](https://docs.rs/mmdbus/)
+
+## ModemManager dbus api
+Dbus bindings to the ModemManager.
+
+Every version corresponds to the ModemManager version [Releases](https://www.freedesktop.org/software/ModemManager/).
+
+[Dbus documentation](https://www.freedesktop.org/software/ModemManager/doc/latest/ModemManager/ref-dbus.html)
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! dbus types for the ModemManager api
+//! with the version 1.6.14
+pub use dbus;
+pub mod bearer;
+pub mod call;
+pub mod modem_firmware;
+pub mod modem_location;
+pub mod modem_messaging;
+pub mod modem_modem3gpp_ussd;
+pub mod modem_modem3gpp;
+pub mod modem_modemcdma;
+pub mod modem_oma;
+pub mod modem_signal;
+pub mod modem_simple;
+pub mod modem_time;
+pub mod modem_voice;
+pub mod modem;
+pub mod sim;
+pub mod sms;
+mod modemmanager;
+pub use modemmanager::*;
